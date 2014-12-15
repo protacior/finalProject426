47b20441516f564692491527dd9dd25a6740ac13
Rachel Protacio <rachel.protacio@yale.edu>
2014-12-14 20:10:48 -0500

    Rachel's preliminary part 3

--- moc_main.cpp
+++ moc_main.cpp
@@ -274,13 +274,93 @@ void FileSharing::shareFiles(FileSharing * _t1)
     void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
     QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
+static const uint qt_meta_data_FingerTable[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: signature, parameters, type, tag, flags
+      13,   12,   12,   12, 0x05,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_FingerTable[] = {
+    "FingerTable\0\0deleteRedundancies()\0"
+};
+
+void FingerTable::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        FingerTable *_t = static_cast<FingerTable *>(_o);
+        switch (_id) {
+        case 0: _t->deleteRedundancies(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData FingerTable::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject FingerTable::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_FingerTable,
+      qt_meta_data_FingerTable, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &FingerTable::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *FingerTable::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *FingerTable::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_FingerTable))
+        return static_cast<void*>(const_cast< FingerTable*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int FingerTable::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void FingerTable::deleteRedundancies()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, 0);
+}
 static const uint qt_meta_data_NetSocket[] = {
 
  // content:
        6,       // revision
        0,       // classname
        0,    0, // classinfo
-      13,   14, // methods
+      14,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -303,6 +383,7 @@ static const uint qt_meta_data_NetSocket[] = {
      265,   10,   10,   10, 0x0a,
      286,  281,   10,   10, 0x0a,
      334,  328,   10,   10, 0x0a,
+     363,   10,   10,   10, 0x0a,
 
        0        // eod
 };
@@ -318,6 +399,7 @@ static const char qt_meta_stringdata_NetSocket[] = {
     "gotRetransmit()\0pair\0"
     "gotStartSearchFor(QPair<QString,quint32>)\0"
     "state\0gotChangedDHTPreference(int)\0"
+    "gotDeleteRedundancies()\0"
 };
 
 void NetSocket::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
@@ -339,6 +421,7 @@ void NetSocket::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
         case 10: _t->gotRetransmit(); break;
         case 11: _t->gotStartSearchFor((*reinterpret_cast< QPair<QString,quint32>(*)>(_a[1]))); break;
         case 12: _t->gotChangedDHTPreference((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 13: _t->gotDeleteRedundancies(); break;
         default: ;
         }
     }
@@ -376,9 +459,9 @@ int NetSocket::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 13)
+        if (_id < 14)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 13;
+        _id -= 14;
     }
     return _id;
 }
